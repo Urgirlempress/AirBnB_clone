9f5cd4a16d6970c8fec86e4b8ffda092ec962952
Bennet-Ukoh <bennetukoh@gmail.com>
2022-11-26 12:32:11 +0000

    task 4 added

--- models/base_model.py
+++ models/base_model.py
@@ -11,8 +11,24 @@ from datetime import datetime
 class BaseModel:
     """Defines common attributes and methods for other classes"""
 
-    def __init__(self):
-        """initializing attributes"""
+          def __init__(self, *args, **kwargs):
+        """Initializes instance attributes
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
+        """
+
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
